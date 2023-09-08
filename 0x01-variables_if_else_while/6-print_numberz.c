741a13f90daa734887d4a743a1bed1852e0d4f5d
Segun Oladapo <segunoladapo.me@gmail.com>
2023-09-08 22:02:53 +0100

    6-print_numberz.c added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
